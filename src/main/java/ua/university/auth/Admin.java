210fac9187b83015ee32583d088f2a73216629ea
dyezovit-stack <d.yezovit@gmail.com>
2026-04-28 09:47:38 +0300

    creating dsign document

---
+++ src/main/java/ua/university/auth/Admin.java
@@ -0,0 +1,4 @@
+package ua.university.auth;
+
+public class Admin {
+}
---
+++ src/test/java/Tests.java
@@ -0,0 +1,8 @@
+import org.junit.jupiter.api.BeforeEach;
+
+public class Tests {
+    @BeforeEach
+
+//По тестам має бути
+
+}
